4b2b86a2ce09039513d98134cb67f47a4ec846fe
“Carina <ksfraan@hotmail.com>
2023-12-18 13:10:29 +0000

    done

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,25 +1,48 @@
-import { useState } from "react";
-
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import '../App.css';
 
 function ProductDetailsPage() {
-  // The state variable `product` is currently an empty object {},
-  // but you should use it to store the response from the Fake Store API (the product details).
-  const [product, setProduct] = useState({});
+    const [product, setProduct] = useState({});
+    const { productId } = useParams();
 
+    useEffect(() => {
+        const fetchProductDetails = async () => {
+            try {
+                const response = await axios.get(
+                    `https://fakestoreapi.com/products/${productId}`
+                );
+                console.log('response: ', response);
+                setProduct(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar detalhes do produto:', error);
+            }
+        };
 
-  // The `productId` coming from the URL parameter is available in the URL path.
-  // You can access it with the `useParams` hook from react-router-dom.
+        fetchProductDetails();
+    }, [productId]);
 
+    return (
+        <div className='ProductDetailsPage'>
+            <img
+                src={product.image}
+                alt={product.title}
+                style={{ width: '15em', height: '15em' }}
+            />
+            <h2 className='product-title'>{product.title}</h2>
+            <p className='product-category'>{product.category}</p>
+            <p className='product-description'>{product.description}</p>
+            <p>
+                <strong>Price:</strong> {product.price}
+            </p>
 
-  // To fetch the product details, set up an effect with the `useEffect` hook:
-
-
-
-  return (
-    <div className="ProductDetailsPage">
-    {/* Render product details here */}
-    </div>
-  );
+            <button>
+                <Link to='/'>Back</Link>
+            </button>
+            {/* */}
+        </div>
+    );
 }
 
 export default ProductDetailsPage;
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,19 +1,45 @@
-import { useState } from "react";
-
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function ProductListPage() {
-  // The state variable `products` is currently an empty array [], 
-  // but you should use it to store the response from the Fake Store API (the list of products).
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  // To fetch the list of products, set up an effect with the `useEffect` hook:
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get(
+                    'https://fakestoreapi.com/products'
+                );
+                console.log('response1: ', response);
+                setProducts(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar produtos:', error);
+            }
+        };
 
+        fetchProducts();
+    }, []);
 
-  return (
-    <div className="ProductListPage">
-      {/* Render list of products here */}
-    </div>
-  );
+    return (
+        <div className='ProductListPage'>
+            <h1>Lista de Produtos</h1>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        <Link to={`/product/details/${product.id}`}>
+                            <img
+                                src={product.image}
+                                alt={product.title}
+                                style={{ width: '50px', marginRight: '10px' }}
+                            />
+                            {product.title} - <strong>${product.price}</strong>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default ProductListPage;
